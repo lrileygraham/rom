fa7255fea72d4df2756440c9e22d0d2af091683b
lrileygraham <lrileygraham@gmail.com>
2014-09-04 09:29:47 -0500

    k

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 resources :ideas
   root :to => redirect('/ideas')
 
-
 Rails.application.routes.draw do
     get 'pages/about'
     
     
+  
 
 
   # The priority is based upon order of creation: first created -> highest priority.
