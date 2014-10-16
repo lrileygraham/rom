e28fd5c83beefe46e1d67b165fd86b3ee43487a8
lrileygraham <lrileygraham@gmail.com>
2014-10-16 10:52:21 -0500

    i

--- app/controllers/photo_controller.rb
+++
@@ -1,9 +0,0 @@
-class PhotoController < ApplicationController
-  def comment
-  end
-  def set_photo
-      @photo = Photo.find(params[:id])
-  end
-  @comments = @photo.comments.all
-  @comment = @photo.comments.build
-end
--- app/models/photo.rb
+++
@@ -1,3 +0,0 @@
-class Photo < ActiveRecord::Base
-    has_many :comments
-end
