d2e35203c4ea589e186aa99a12f2bc35e66224e5
lrileygraham <lrileygraham@gmail.com>
2014-10-16 11:53:40 -0500

    o

--- db/migrate/20141016165153_create_photos.rb
+++ db/migrate/20141016165153_create_photos.rb
@@ -1,6 +1,9 @@
 class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
+      t.string :name
+      t.text :description
+      t.string :picture
 
       t.timestamps
     end
